1f5e43e33010bf3d601f752c5f59a8a89e391f21
zyuiop <zyuiop@zyuiop.net>
2019-05-02 23:42:52 +0200

    Remove old get_ticket endpoint

--- app/controllers/api/AppTicketController.scala
+++ app/controllers/api/AppTicketController.scala
@@ -20,35 +20,17 @@ class AppTicketController @Inject()(cc: ControllerComponents,
                                     tickets: TicketsModel,
                                     groups: GroupsModel)(implicit ec: ExecutionContext, apps: AppsModel, mailer: MailerClient, config: Configuration) extends AbstractController(cc) {
 
-
-  @deprecated
-  def postAppTicket: Action[AppTicketRequest] = Action.async(parse.json[AppTicketRequest]) { implicit rq =>
-    if (rq.hasBody) {
-      val body = rq.body
-
-      ApiUtils.withGivenApp(body.clientId, body.clientSecret) { app =>
-        returnTicket(app, body.ticket)
-      }
-    }
-    else !MissingData // No body or body parse fail ==> invalid input
-  }
-
-  private def returnTicket(app: data.App, ticket: String): Future[Result] = {
-    tickets useTicket(ticket, app) flatMap {
-      case Some((data.Ticket(_, _, _, validTo, ticketType), data.RegisteredUser(Some(id), email, _, _, _, _, _, _))) =>
-        // Found some ticket bound to the app
-        if (validTo.before(new Date)) !InvalidTicket // The ticket is no longer valid
-        else {
-          groups getGroups(app, id) map (groupSet => AppTicketResponse(id, email, ticketType, groupSet))
-        } // The ticket is valid, return the data
-      case None => !InvalidTicket
-    }
-  }
-
   def getAppTicket(ticket: String): Action[AnyContent] = Action.async { implicit rq =>
     ApiUtils.withApp { app =>
-      returnTicket(app, ticket)
-    }
+      tickets useTicket(ticket, app) flatMap {
+        case Some((data.Ticket(_, _, _, validTo, ticketType), data.RegisteredUser(Some(id), email, _, _, _, _, _, _))) =>
+          // Found some ticket bound to the app
+          if (validTo.before(new Date)) !InvalidTicket // The ticket is no longer valid
+          else {
+            groups getGroups(app, id) map (groupSet => AppTicketResponse(id, email, ticketType, groupSet))
+          } // The ticket is valid, return the data
+        case None => !InvalidTicket
+      }    }
   }
 
 }
