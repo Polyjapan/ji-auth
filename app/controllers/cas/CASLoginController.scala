ebe8c49eab2f256c194448c54a62795b6e92f475
Louis Vialar <louis.vialar@gmail.com>
2020-04-02 20:12:45 +0200

    CAS Login parameters

--- app/controllers/cas/CASLoginController.scala
+++ app/controllers/cas/CASLoginController.scala
@@ -7,17 +7,28 @@ import play.api.mvc._
 import play.twirl.api.Html
 
 import scala.concurrent.ExecutionContext
+import data.UserSession._
 
 /**
  * @author Louis Vialar
  */
 class CASLoginController @Inject()(cc: ControllerComponents, apps: ServicesModel)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
-  def loginGet(service: String): Action[AnyContent] = Action.async { implicit rq =>
+  def loginGet(service: String, renew: Option[Boolean], gateway: Option[Boolean]): Action[AnyContent] = Action.async { implicit rq =>
     apps.getCasService(service).map {
       case Some(casService) =>
-        Redirect(controllers.routes.RedirectController.redirectGet())
-          .addingToSession(CASInstance(url = casService.serviceRedirectUrl.getOrElse(service), casService.serviceId).pair)
+        if (gateway.getOrElse(false) && !rq.hasUserSession) {
+          // Gateway: if param is set, we should not attempt to log the user in
+          Redirect(casService.serviceRedirectUrl.getOrElse(service))
+        } else {
+          val ret = Redirect(controllers.routes.RedirectController.redirectGet())
+
+          if (renew.getOrElse(false))
+            // Renew: if param is set, we should drop the existing user session and ask the user to log in again
+            ret.withSession(CASInstance(url = casService.serviceRedirectUrl.getOrElse(service), casService.serviceId).pair)
+          else
+            ret.addingToSession(CASInstance(url = casService.serviceRedirectUrl.getOrElse(service), casService.serviceId).pair)
+        }
       case None => Ok(views.html.errorPage("Service introuvable", Html("<p>Le service spécifié est introuvable. Merci de signaler cette erreur au créateur du site dont vous provenez.")));
     }
   }
